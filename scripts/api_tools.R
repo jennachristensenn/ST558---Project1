d1bb0c0e7e6231dc82ce42f6ae268a1cb42b2c06
Matt Wasyluk <mwasylu@ncsu.edu>
2024-09-29 19:44:50 -0400

    reintroduce geo vars

--- scripts/api_tools.R
+++ scripts/api_tools.R
@@ -6,23 +6,27 @@ library(tibble)
 
 PUMS_URL_MAIN_STUB <- "https://api.census.gov/data/"
 PUMS_URL_ACS_STUB <- "/acs/acs1/pums"
-PUMS_URL_QUERYSTRING_STUB <- "?get=PWGTP"
+PUMS_URL_QUERYSTRING_STUB <- "?for=state:02&get=PWGTP"
 
 DEFAULT_YEARS <- c(2022)
 #PWGTP is always included so is in base querystring stub
 DEFAULT_NUM_VARS <- c("AGEP")
 DEFAULT_CAT_VARS <- c("SEX")
+ALL_GEO <- c("REGION", "DIVISION", "ST")
+DEFAULT_GEO_VARS <- c("ST")
 
 #account for lack of 2020 data on site, then compare years as character for consistency
 AVAILABLE_YEARS <- as.character(c(seq(2010, 2019), seq(2021, 2022)))
 AVAILABLE_NUM_VARS <- c("AGEP", "GASP", "GRPIP", "JWAP", "JWDP", "JWMNP")
 AVAILABLE_CAT_VARS <- c("FER", "HHL", "HISPEED", "JWAP", "JWDP", "JWTRNS", "SCH", "SCHL", "SEX")
+AVAILABLE_GEO_VARS <- c("REGION", "DIVISION", "ST", "ALL_GEO")
  
 fetch_census_raw <- function(year=2022, varstring=""){
   
   prepared_census_url <- paste(PUMS_URL_MAIN_STUB, year, PUMS_URL_ACS_STUB, PUMS_URL_QUERYSTRING_STUB, sep = "")
   if(nchar(varstring) > 0){prepared_census_url <-  paste(prepared_census_url, varstring, sep = ",")}
     
+  print(prepared_census_url)
   return(GET(prepared_census_url))
 }
 
@@ -34,7 +38,7 @@ parse_census_response <- function(census_raw){
   return(census_tbl)
 }
 
-fetch_census_data <- function(years=DEFAULT_YEARS, num_vars=DEFAULT_NUM_VARS, cat_vars=DEFAULT_CAT_VARS){
+fetch_census_data <- function(years=DEFAULT_YEARS, num_vars=DEFAULT_NUM_VARS, cat_vars=DEFAULT_CAT_VARS, geo_vars = DEFAULT_GEO_VARS){
   num_vars_checked <- num_vars[num_vars %in% AVAILABLE_NUM_VARS]
   num_vars_failed <- num_vars[!(num_vars %in% AVAILABLE_NUM_VARS)]
   if(length(num_vars_failed > 0)){warning("Invalid numeric variable(s) excluded: ", paste(num_vars_failed))}
@@ -59,17 +63,25 @@ fetch_census_data <- function(years=DEFAULT_YEARS, num_vars=DEFAULT_NUM_VARS, ca
     years_checked = DEFAULT_CAT_VARS
   }
   
-  querystring_var_list <-  paste(num_vars_checked, cat_vars_checked, sep = ",")
+  geo_vars_checked <- geo_vars[geo_vars %in% AVAILABLE_GEO_VARS]
+  geo_vars_failed <- geo_vars[!(geo_vars %in% AVAILABLE_GEO_VARS)]
+  if(length(geo_vars_failed) > 0){warning("Invalid geographic variable(s) excluded: ", paste(geo_vars_failed))}
+  if(length(geo_vars_checked) == 0){
+    warning("No valid geographic variables supplied. Using default ALL.")
+    geo_vars_checked = DEFAULT_GEO_VARS
+  }
+  
+  querystring_var_list <-  paste(num_vars_checked, cat_vars_checked, geo_vars_checked, sep = ",")
   
   fetch_census_single_year <- function(year){
-    current_iter_response <- fetch_census_raw(varstring = querystring_var_list)$content |> 
+    current_iter_response <- fetch_census_raw(year=year, varstring = querystring_var_list)$content |> 
       parse_census_response()
     
     current_iter_response$YEAR <- year
     return(current_iter_response)
   }
   
-  return(map_dfr(years, fetch_census_single_year))
+  return(map_dfr(years_checked, fetch_census_single_year))
 }
 
 test <- fetch_census_data(num_vars = c("ABCD"), cat_vars = c("FER"), years = c(2012, 2014, 2024))
